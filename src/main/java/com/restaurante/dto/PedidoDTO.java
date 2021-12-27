29430d1d4895577567384a51564137d950bea7a8
mbjunqueiraweb <mauro.junqueira@mercadolivre.com.br>
2021-12-27 14:36:36 -0300

    dtos

--- src/main/java/com/restaurante/dto/PedidoDTO.java
+++ src/main/java/com/restaurante/dto/PedidoDTO.java
@@ -1,4 +1,54 @@
 package com.restaurante.dto;
 
+import com.restaurante.entity.Mesa;
+import com.restaurante.entity.Pedido;
+import com.restaurante.entity.Prato;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class PedidoDTO {
+    private Integer id;
+    private List<Prato> listaPratos;
+    private Mesa mesa;
+    private BigDecimal valorTotal;
+
+
+    public static Pedido converte(PedidoDTO dto) {
+        Pedido pedido = Pedido.builder()
+
+                .id(dto.getId())
+                .listaPratos(dto.getListaPratos())
+                .mesa(dto.getMesa())
+                .valorTotal(dto.getValorTotal())
+                .build();
+
+        return pedido;
+    }
+
+    public static PedidoDTO converte(Pedido pedido) {
+        return PedidoDTO.builder()
+                .id(pedido.getId())
+                .mesa(pedido.getMesa())
+                .valorTotal(pedido.getValorTotal())
+                .build();
+    }
+
+    public  static List<PedidoDTO> converte(List<Pedido> pedido) {
+        return pedido.stream().map(u -> converte(u)).collect(Collectors.toList());
+
+    }
+
 }
+
+
